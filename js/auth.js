899a5a9d1197c9b44f06f08387212e940f398404
GeorginaRetaLimas <2430043@upv.edu.mx>
2025-10-16 07:08:21 -0600

    Validacion de datos de registro Usuario

--- js/auth.js
+++ js/auth.js
@@ -21,54 +21,50 @@ document.addEventListener("DOMContentLoaded", function(){
         usuarios = [];
     }
 
-    document.getElementById('form_Inicio').addEventListener('submit', function(e) {
-        e.preventDefault();
-    });
-
-    // Cuando se envie el formulario de registro
-    document.getElementById('form_Registro').addEventListener('submit', function(e) {
-        e.preventDefault();
-
-        if(validarRegistro){
-
-        }
-    });
-
-
 });
 
-function validarRegistro() {
+document.getElementById('form_Inicio').addEventListener('submit', function(e) {
+    e.preventDefault();
+});
+
+// Cuando se envie el formulario de registro
+document.getElementById('form_Registro').addEventListener('submit', function(e) {
+    e.preventDefault();
+
     const nombreUsuario = document.getElementById("nombre_Usuario").value;
     const correo = document.getElementById("correo_registro").value;
-    const contraseña = document.getElementById("contraseña_registro");
-    const confirmar = document.getElementById("confirmar_registro");
+    const contraseña = document.getElementById("contraseña_registro").value;
+    const confirmar = document.getElementById("confirmar_registro").value;
+
+    console.log(nombreUsuario, correo, contraseña, confirmar);
 
     if (!nombreUsuario || !correo || !contraseña || !confirmar) {
         Swal.fire('Advertencia', 'Rellene todos los campos primero', 'warning');
-        return false;
+        return;
     }
 
     if (contraseña !== confirmar) {
         Swal.fire('Error', 'Las contraseñas no coinciden', 'error');
-        return false;
+        return;
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(correo)) {
         Swal.fire('Error', 'Ingrese un correo electrónico válido', 'error');
-        return false;
+        return;
     }
 
     if (typeof usuarios !== 'undefined') {
         const usuarioExistente = usuarios.find(user => user.correo === correo);
         if (usuarioExistente) {
             Swal.fire('Error', 'Este correo ya está registrado', 'error');
-            return false;
+            return;
         }
     }
 
-    return true;
-}
+    
+});
+
 
 document.getElementById('link_registro').addEventListener('click', function() {
     document.getElementById('vista_inicio').classList.add('d-none');
