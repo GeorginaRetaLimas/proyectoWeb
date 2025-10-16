1cf70dc6e317b1ed151d0cd7b4f88a33300d6568
GeorginaRetaLimas <2430043@upv.edu.mx>
2025-10-16 07:51:57 -0600

    Registro de usuarios exitoso

--- js/auth.js
+++ js/auth.js
@@ -62,8 +62,27 @@ document.getElementById('form_Registro').addEventListener('submit', function(e) 
         }
     }
 
-    
+    registrarUsuario(nombreUsuario, correo, contraseña);
 });
+
+function registrarUsuario(nombreUsuario, correo, contraseña){
+    console.log("Creando nuevo usario: ", nombreUsuario);    
+
+    const id = usuarios.length > 0 ? Math.max(...usuarios.map(u => u.id_usuario)) + 1 : 1;
+    
+    const nuevoUsuario = {
+        id_usuario: id,
+        username: nombreUsuario,
+        correo: correo,
+        contraseña: contraseña,
+        rol: "usuario" 
+    };
+
+    usuarios.push(nuevoUsuario);
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    Swal.fire('Éxito', 'Usuario registrado con éxito', 'success');
+}
 
 
 document.getElementById('link_registro').addEventListener('click', function() {
